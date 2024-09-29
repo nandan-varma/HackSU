a469cf8c39757999e3878a4c97de17f66d44f294
Samuriot <92127962+Samuriot@users.noreply.github.com>
2024-09-29 15:50:49 -0700

    added routing to About and Theme pages

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='About'>
+        <h1>Hello World!</h1>
+        <p>Lorem Ipsum...
+        </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Theme/Theme.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Theme = () => {
+  return (
+    <div className='Theme'>
+        <h1>Hello World!</h1>
+        <h2>Here are our themes for this year!</h2>
+    </div>
+  )
+}
+
+export default Theme

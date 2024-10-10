7703d55e50e5686159920f7fa4c8744f41021012
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2024-10-10 05:40:17 +0000

    added theme details and slight update to FAQ

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -22,13 +22,23 @@ const Faq = () => {
     }
   ];
 
+  const [activeIndex, setActiveIndex] = React.useState(null);
+
+  const toggleAnswer = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
     <div className='faq-container'>
       <h1 className='faq-title'>FAQ</h1>
       {faqs.map((faq, index) => (
         <div key={index} className='faq-item'>
-          <h2 className='faq-question'>{faq.question}</h2>
-          <p className='faq-answer'>{faq.answer}</p>
+          {/* TODO: add animations maybe */}
+          <h2 className='faq-question' onClick={() => toggleAnswer(index)}>
+            {faq.question}
+            <svg className={`svg ${activeIndex === index ? "rotate" : ""}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" /></svg>
+          </h2>
+          {activeIndex === index && <p className='faq-answer'>{faq.answer}</p>}
         </div>
       ))}
     </div>
--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import './Theme.css'
 
 const Theme = () => {
   return (
     <div className='Theme'>
-        <h1>Hello World!</h1>
-        <h2>Here are our themes for this year!</h2>
+      <h1 className='Theme-title'>We are <b>Better Together!</b></h1>
+      <div className='Theme-blocks'>
+        <div className='Theme-section'>
+          <h2 className='Theme-subtitle'>Human Connectivity</h2>
+          <div className='Theme-content'>
+            <ul className='Theme-list'>
+              <li className='Theme-listItem'>Applications surrounded on the basis of community and social opportunities</li>
+              <li className='Theme-listItem'>Fostering communication, interpersonal relationships, and respect for others</li>
+              <li className='Theme-listItem'>Encouraging social interactions, care for one another, etc.</li>
+              <li className='Theme-listItem'>Mentors from SEAC, MOSAIC, and other companies fostering connection!</li>
+            </ul>
+          </div>
+        </div>
+        <div className='Theme-section'>
+          <h2 className='Theme-subtitle'>Applied Science & HealthCare</h2>
+          <div className='Theme-content'>
+            <ul className='Theme-list'>
+              <li className='Theme-listItem'>Applications based on personal or communal health</li>
+              <li className='Theme-listItem'>Applications solving issues within respective fields of healthcare and the sciences</li>
+              <li className='Theme-listItem'>Innovative solutions to unique and pressing issues</li>
+              <li className='Theme-listItem'>Mentors from Biology Club, Psi Chi, and other specialties!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
